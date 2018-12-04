ec0febe510175877230a9e741bc1b7c67e804ec2
AdriK12 <41646675+AdriK12@users.noreply.github.com>
2018-12-04 06:17:50 +0100

    Update Estacion.java

--- 2018/src/Estacion.java
+++ 2018/src/Estacion.java
@@ -1,16 +1,21 @@
 public class Estacion{
 
+  private String nombre;
   private int numeroEstaciones;
   private Estacion[] estaciones;
-  private int linea;
+  private int numeroLineas;
+  private int[] lineas;
   
-  public Estacion(int numeroEstaciones, Estacion[] estaciones, int linea){
+  public Estacion(String nombre, int numeroEstaciones, Estacion[] estaciones, int numeroLineas, int[] lineas){
+    this.nombre=nombre;
     this.numeroEstaciones=numeroEstaciones;
     this.estaciones=estaciones;
-    this.linea=linea;
+    this.numeroLineas=numeroLineas;
+    this.lineas=lineas;
   }
   
-  public Estacion(Estacion est1, Estacion est2, Estacion est3, Estacion est4, int linea){
+  public Estacion(String nombre, Estacion est1, Estacion est2, Estacion est3, Estacion est4, int lin1, int lin2){
+    this.nombre=nombre;
     numeroEstaciones=0;
     aux={est1, est2, est3, est4};
     for(int i=0; i<aux.length; i++){
@@ -26,7 +31,24 @@ public class Estacion{
         cont++;
       }
     }
-    this.linea=linea;
+    numeroLineas=0;
+    aux={lin1, lin2}
+    for(int i=0; i<aux.length; i++){
+      if(aux[i]!=null){
+        numeroLineas++;
+      }
+      lineas=new int[numeroLineas];
+      cont=0;
+      for(int i=0; i<aux.length; i++){
+      if(aux[i]!=null){
+        lineas[cont]=aux[i];
+        cont++;
+      }
+    }
+  }
+  
+  public String getNombre(){
+    return nombre;
   }
   
   public int getNumeroEstaciones(){
@@ -37,8 +59,12 @@ public class Estacion{
     return estaciones;
   }
   
-  public int getLinea(){
-    return linea;
+  public int getNumeroLineas(){
+    return numeroLineas;
+  }
+    
+  public int[] getLineas(){
+    return lineas;
   }
   
 }
