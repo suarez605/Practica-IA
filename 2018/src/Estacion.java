8e5a602b2b041279316bbe8d1d80396a44433fdc
AdriK12 <41646675+AdriK12@users.noreply.github.com>
2018-12-05 12:50:30 +0100

    Update Estacion.java

--- 2018/src/Estacion.java
+++ 2018/src/Estacion.java
@@ -1,48 +1,31 @@
+import java.util.ArrayList;
+
 public class Estacion{
 
   private String nombre;
-  private int numeroEstaciones;
-  private Estacion[] estaciones;
-  private int numeroLineas;
-  private int[] lineas;
+  private ArrayList<Estacion> estaciones;
+  private ArrayList<Integer> lineas;
   
-  public Estacion(String nombre, int numeroEstaciones, Estacion[] estaciones, int numeroLineas, int[] lineas){
+  public Estacion(String nombre, ArrayList<Estacion> estaciones, ArrayList<Integer> lineas){
     this.nombre=nombre;
-    this.numeroEstaciones=numeroEstaciones;
     this.estaciones=estaciones;
-    this.numeroLineas=numeroLineas;
     this.lineas=lineas;
   }
   
   public Estacion(String nombre, Estacion est1, Estacion est2, Estacion est3, Estacion est4, int lin1, int lin2){
     this.nombre=nombre;
-    numeroEstaciones=0;
+    estaciones=new ArrayList<Estacion>();
+    lineas=new ArrayList<Integer>(); 
     Estacion[] aux={est1, est2, est3, est4};
     for(int i=0; i<aux.length; i++){
       if(aux[i]!=null){
-        numeroEstaciones++;
+        estaciones.add(aux[i]);
       }
     }
-    estaciones=new Estacion[numeroEstaciones];
-    int cont=0;
-    for(int i=0; i<aux.length; i++){
-      if(aux[i]!=null){
-        estaciones[cont]=aux[i];
-        cont++;
-      }
-    }
-    numeroLineas=0;
-    int[] aux2={lin1, lin2}
+    int[] aux2={lin1, lin2};
     for(int i=0; i<aux2.length; i++){
       if(aux2[i]!=null){
-        numeroLineas++;
-      }
-      lineas=new int[numeroLineas];
-      cont=0;
-      for(int i=0; i<aux2.length; i++){
-      if(aux2[i]!=null){
-        lineas[cont]=aux2[i];
-        cont++;
+        lineas.add(aux2[i]);
       }
     }
   }
@@ -51,19 +34,11 @@ public class Estacion{
     return nombre;
   }
   
-  public int getNumeroEstaciones(){
-    return numeroEstaciones;
-  }
-  
-  public Estacion[] getEstaciones(){
+  public ArrayList<Estacion> getEstaciones(){
     return estaciones;
   }
-  
-  public int getNumeroLineas(){
-    return numeroLineas;
-  }
     
-  public int[] getLineas(){
+  public ArrayList<Integer> getLineas(){
     return lineas;
   }
   
