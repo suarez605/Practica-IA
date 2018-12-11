19174eef9bcdd7f2a27e67ad24ba2795044f6a3d
AdriK12 <41646675+AdriK12@users.noreply.github.com>
2018-12-11 11:50:52 +0100

    Update AEstrella.java

--- 2018/src/AEstrella.java
+++ 2018/src/AEstrella.java
@@ -2,6 +2,26 @@ import java.util.ArrayList;
 
 public class AEstrella {
 
-  
+  public ArrayList<Estacion> aEstrella(Estacion[] estaciones, Estacion origen, Estacion destino){
+    if(origen.getLinea()==destino.getLinea()){
+      Estacion actual=origen;
+      Estacion anterior=origen.getEstaciones.get(1);
+      ArrayList<Estacion> result=new ArrayList<Estacion>();
+      boolean final=false;
+      while(!final){
+        result.add(actual);
+        for(int i=0;i<actual.getEstaciones().size();i++){
+          if(!actual.getEstaciones().get(i).getNombre().equals(anterior.getNombre())&&actual.getEstaciones().get(i).getLinea()==actual.getLinea()){
+            anterior=new;
+            actual=actual.getEstaciones().get(i);
+          }
+        }
+        
+      }
+    }
+    else{
+      
+    }
+  }
   
 }
