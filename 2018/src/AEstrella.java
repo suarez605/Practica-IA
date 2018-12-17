1dd909ad38c5d92c4b6775ee49b72223c58e07ae
AdriK12 <41646675+AdriK12@users.noreply.github.com>
2018-12-17 02:16:37 +0100

    Update AEstrella.java

--- 2018/src/AEstrella.java
+++ 2018/src/AEstrella.java
@@ -71,7 +71,6 @@ public class AEstrella {
 			boolean aux=false;
 			boolean aux2=false;
 			boolean prohibidoAux=false;
-			boolean repetidoAux=false;
 			int i=0;
 			int j=0;
 
@@ -84,7 +83,6 @@ public class AEstrella {
 						if(!anterior.getNombre().equals(actual.getEstaciones()[i])){
 							aux2=false;
 							prohibidoAux=false;
-							repetidoAux=false;
 							j=0;
 
 							for(int k=0;k<prohibidas.size();k++){
@@ -92,14 +90,8 @@ public class AEstrella {
 									prohibidoAux=true;
 								}
 							}
-							
-							for(int k=0;k<result.size();k++){
-								if(actual.getEstaciones()[i].equals(result.get(k).getNombre())){
-									repetidoAux=true;
-								}
-							}
 
-							if(!prohibidoAux&&!repetidoAux){
+							if(!prohibidoAux){
 								while(!aux2){
 									String[] aux3 = actual.getEstaciones();
 									if(estaciones[j].getNombre().equals(aux3[i])){
@@ -156,8 +148,90 @@ public class AEstrella {
 					actual=origen;
 				}
 			}
-
 			
+			result.clear();
+			Estacion origenAux=new Estacion("",null,null,null,-1);
+			anterior=new Estacion("",null,null,null,-1);
+			prohibida=new Estacion("",null,null,null,-1);
+			inicio=true;
+			vfinal=false;
+			aux=false;
+			aux2=false;
+			i=0;
+			j=0;
+			
+			for(int h=0;h<2;h++){
+				
+				if(h==0){
+					actual=resultaux.get(resultaux.size()-1);
+					origenAux=actual;
+					resultaux.remove(resultaux.size()-1);
+				}
+				if(h==1){
+					actual=resultaux2.get(resultaux2.size()-1);
+					origenAux=actual;
+					resultaux2.remove(resultaux2.size()-1);
+				}
+				
+				while(!vfinal){
+					result.add(actual);
+					
+					aux=false;
+					i=0;
+					while(!aux){
+						if(!prohibida.getNombre().equals(actual.getEstaciones()[i])&&!anterior.getNombre().equals(actual.getEstaciones()[i])){
+							aux2=false;
+							j=0;
+							while(!aux2){
+								String[] aux3 = actual.getEstaciones();
+								if(estaciones[j].getNombre().equals(aux3[i])){
+									aux2=true;
+									if(estaciones[j].getLinea()==actual.getLinea()){
+										anterior=actual;
+										actual=estaciones[j];
+										aux=true;
+										if(inicio){
+											prohibida=actual;
+											inicio=false;
+										}
+									}
+								}
+								j++;
+								if(j>=estaciones.length){
+									aux2=true; 
+								}
+							}
+						}
+						if(i==actual.getEstaciones().length-1&&!aux){
+							result.clear();
+							actual=origenAux;
+							aux=true;
+						}
+						i++;
+					}
+					if(actual.getNombre().equals(destino.getNombre())){
+						result.add(actual);
+						vfinal=true;
+					}
+				}
+				
+				if(h==0){
+					for(int k=0;k<result.size();k++){
+						resultaux.add(result.get(k));
+					}
+					vfinal=false;
+					result.clear();
+					inicio=true;
+				}
+				
+				if(h==1){ 
+					for(int k=0;k<result.size();k++){
+						resultaux2.add(result.get(k));
+					}
+					result.clear();
+				}
+				
+			}
 
 			if(resultaux.size()>=resultaux2.size()){
 				for(int n=0;n<resultaux2.size();n++){
